db90c6c5e97813b327f71eab426678edd85c7923
Kunal <kdshah6593@gmail.com>
2021-03-25 15:35:02 -0400

    commit tree a969bb13755bcab2fba62bc3592ac185bc37451a

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,16 @@
-export function manageFriends(state, action){
+export function manageFriends(state = {friends: []}, action){
+    switch(action.type) {
+        case 'ADD_FRIEND':
+            return {...state, friends: [...state.friends, action.friend]} ;
+        case 'REMOVE_FRIEND':
+            const remFriend = state.friends.find(friend => friend.id === action.id)
+            const remIndex = state.friends.indexOf(remFriend)
+            return {...state, friends: [
+                ...state.friends.slice(0, remIndex), 
+                ...state.friends.slice(remIndex + 1)    
+            ]
+        }
+        default:
+            return state;
+    }
 }
